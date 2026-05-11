acdd8e0015dd361816f5e4d00bad741d732a004b
Jon-SOScial <jonleecole@gmail.com>
2026-05-11 15:24:36 +0000

    Add client-side module registration for /directory/nearby

---
+++ static/lib/nearby.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Client-side module for the /directory/nearby page
+// All functionality is in the template's inline script
+// This file exists only to prevent NodeBB's ajaxify from erroring
+
+define('forum/nearby', ['jquery'], function($) {
+	var Nearby = {};
+	// Template handles all logic via inline script
+	return Nearby;
+});
