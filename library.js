57fc0cdc4624d51bf6f43b69eaa71a7718524c1e
Jon-SOScial <jonleecole@gmail.com>
2026-05-11 15:53:53 +0000

    Fix: Use fs.readFileSync to bypass view engine

--- library.js
+++ library.js
@@ -1,18 +1,26 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const plugin = {};
 
 plugin.init = function(params, callback) {
 	const app = params.app;
-	
-	// Register the /directory/nearby route
 	app.get('/directory/nearby', plugin.nearbyController);
-	
 	callback();
 };
 
 plugin.nearbyController = function(req, res) {
-	res.render('plugins/nodebb-plugin-waymker-geo/nearby', {});
+	// Read the template file directly
+	const templatePath = path.join(__dirname, 'static/templates/nearby.tpl');
+	
+	try {
+		const html = fs.readFileSync(templatePath, 'utf8');
+		res.send(html);
+	} catch (err) {
+		console.error('[waymker-geo] Failed to read template:', err);
+		res.status(500).send('Failed to load nearby page: ' + err.message);
+	}
 };
 
 plugin.loadGeoFeatures = function(data, callback) {
@@ -23,7 +31,6 @@ plugin.loadGeoFeatures = function(data, callback) {
 	const leafletMarkerJS = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/leaflet.markercluster.js';
 
 	data.scripts = data.scripts || [];
-	
 	data.scripts.push({ src: leafletCSS, attribute: 'rel="stylesheet"' });
 	data.scripts.push({ src: leafletMarkerCSS, attribute: 'rel="stylesheet"' });
 	data.scripts.push({ src: leafletMarkerDefaultCSS, attribute: 'rel="stylesheet"' });
